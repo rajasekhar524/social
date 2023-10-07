e6ac874baaa22fec250b409330524494baf58018
Rajasekhar524 <rajshekhar.k321@gmail.com>
2023-10-08 02:56:37 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
-app.get("/hello",()=>{
+app.get("/hello",(req,res)=>{
   res.json("hello")
 })
 
