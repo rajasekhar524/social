37a43d125441122d35c822f799d83a0cab31948c
Rajasekhar524 <rajshekhar.k321@gmail.com>
2023-10-08 02:54:43 +0530

    first commit

--- server/index.js
+++ server/index.js
@@ -51,6 +51,9 @@ app.post("/posts", verifyToken, upload.single("picture"), createPost);
 app.use("/auth", authRoutes);
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+app.get("/hello",()=>{
+  res.json("hello")
+})
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
